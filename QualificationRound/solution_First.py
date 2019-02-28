1bcc9d67f988afc6be6bb1eefaac4af67bfe599e
Nattapong Ousirimaneechai <chai-best@hotmail.com>
2019-03-01 01:58:17 +0700

    Change: logic no shuffle v_pics and h_pics

--- QualificationRound/solution_First.py
+++ QualificationRound/solution_First.py
@@ -6,31 +6,35 @@ def merge(pic_1, pic_2):
         'tags': pic_1['tags'] | pic_2['tags'],
     }
 
-f = open('QualificationRound/a_example.txt')
-n = int(f.readline())
-h_pics, v_pics = [], []
-for i in range(n):
-    tags = f.readline().strip().split()
-    pic_type, num_tags, tags = tags[0], int(tags[1]), set(tags[2:])
-    if pic_type == 'H':
-        h_pics.append({
-            'id': [str(i)],
-            'tags': tags,
-        })
-    elif pic_type == 'V':
-        v_pics.append({
-            'id': [str(i)], 
-            'tags': tags
-        })
+sol_folder = 'random_sol2'
+datasets = ['a_example', 'b_lovely_landscapes', 'c_memorable_moments', 'd_pet_pictures', 'e_shiny_selfies']
 
-shuffle(v_pics)
-v_pics = [merge(v_pics[2 * i], v_pics[2 * i + 1]) for i in range(len(v_pics) // 2)]
+for dataset in datasets:
+    f = open('QualificationRound/{}.txt'.format(dataset))
+    n = int(f.readline())
+    h_pics, v_pics = [], []
+    for i in range(n):
+        tags = f.readline().strip().split()
+        pic_type, num_tags, tags = tags[0], int(tags[1]), set(tags[2:])
+        if pic_type == 'H':
+            h_pics.append({
+                'id': [str(i)],
+                'tags': tags,
+            })
+        elif pic_type == 'V':
+            v_pics.append({
+                'id': [str(i)], 
+                'tags': tags
+            })
 
-slides = h_pics + v_pics
-shuffle(slides)
+    shuffle(v_pics)
+    v_pics = [merge(v_pics[2 * i], v_pics[2 * i + 1]) for i in range(len(v_pics) // 2)]
 
-wf = open('QualificationRound/solution_First.txt', 'w')
-wf.write('{}\n'.format(len(slides)))
-for slide in slides:
-    wf.write('{}\n'.format(' '.join(slide['id'])))
-wf.close()
+    slides = h_pics + v_pics
+    # shuffle(slides)
+
+    wf = open('QualificationRound/{}/{}.txt'.format(sol_folder, dataset), 'w')
+    wf.write('{}\n'.format(len(slides)))
+    for slide in slides:
+        wf.write('{}\n'.format(' '.join(slide['id'])))
+    wf.close()
